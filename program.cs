9ac506c842f170b4c2f81785d7da137f334a34f5
Farid_mr <itmirengineering@gmail.com>
2023-05-31 23:25:39 +0330

    Update userTask

--- program.cs
+++ program.cs
@@ -1,18 +1,30 @@
 using System;
 
-namespace toDoList{
-    class user{
+namespace toDoList
+{
+    class user
+    {
         protected string username;
     }
     class task
     {
         protected bool is_done;
     }
-    class userTask{
-        protected string task;
+    class userTask
+    {
+        protected string order;
+
+        public userTask(string order)
+        {
+
+
+
+        }
     }
-    class program{
-        static function Main(string[] args){
+    class program
+    {
+        static function Main(string[] args)
+        {
             Console.WriteLine("-----");
             Console.WriteLine("-----");
         }
