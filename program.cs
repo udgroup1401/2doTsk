fc12b17cb00885d751cb73ae0c48937cf94ebcd9
saeed <sydr16156@gmail.com>
2023-05-31 23:26:58 +0330

    update

--- program.cs
+++ program.cs
@@ -6,7 +6,23 @@ namespace toDoList{
     }
     class task
     {
+        protected string name;
+        protected int taskid;
+        protected string task_description;
         protected bool is_done;
+        protected int dtstart;
+        protected int dtend;
+        protected string tag;
+        public task(string name,int taskid, string task_description, bool is_done, int dtstart, int dtend, string tag)
+        {
+        this.name=name;
+        this.taskid=taskid;
+        this.task_description=task_description;
+        this.is_done=is_done;
+        this.dtstart=dtstart;
+        this.dtend=dtend;
+        this.tag=tag;
+        }
     }
     class userTask{
         protected string task;
