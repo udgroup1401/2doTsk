aa58f29ef04fd10120e62451e2c84b790f123aab
Farid_mr <itmirengineering@gmail.com>
2023-06-01 16:03:28 +0330

    updated class userTask and jsondb

--- program.cs
+++ program.cs
@@ -4,25 +4,31 @@ using System.IO;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
-namespace toDoList{
-    class user{
+namespace toDoList
+{
+    class user
+    {
         protected string username;
         protected string password;
         protected bool loggedIn;
-        public user(string u,string p){
+        public user(string u, string p)
+        {
             this.username = u;
             this.password = p;
             this.loggedIn = false;
         }
-        public void register(){
+        public void register()
+        {
             string jsonContent = File.ReadAllText("db.json");
             JObject dbFile = JObject.Parse(jsonContent);
             List<string[]> allUsersAndPassword = new List<string[]>();
-            foreach (var user in dbFile["users"]){
+            foreach (var user in dbFile["users"])
+            {
                 allUsersAndPassword.Add(new string[] { user["username"].ToString(), user["password"].ToString() });
             }
             bool isValidUser = allUsersAndPassword.Exists(arr => arr[0] == this.username);
-            if(isValidUser == false){
+            if (isValidUser == false)
+            {
                 var newResultToAdd = new JObject(
                     new JProperty("username", this.username),
                     new JProperty("password", this.password),
@@ -37,21 +43,23 @@ namespace toDoList{
                 this.loggedIn = true;
             }
         }
-        public void login(){
+        public void login()
+        {
             List<string[]> allUsersAndPassword = new List<string[]>();
             string jsonContent = File.ReadAllText("db.json");
             dynamic dbFile = JsonConvert.DeserializeObject(jsonContent);
-            foreach (var user in dbFile.users){
+            foreach (var user in dbFile.users)
+            {
                 allUsersAndPassword.Add(new string[] { user.username.ToString(), user.password.ToString() });
             }
             bool isValidUser = allUsersAndPassword.Exists(arr => arr[0] == this.username && arr[1] == this.password);
             this.loggedIn = isValidUser;
         }
-        
 
-        public string getUsername(){ return this.username; }
-        public bool isLogin(){ return this.loggedIn; }
-        public void logout(){ this.loggedIn = false; }
+
+        public string getUsername() { return this.username; }
+        public bool isLogin() { return this.loggedIn; }
+        public void logout() { this.loggedIn = false; }
     }
     class task
     {
@@ -62,56 +70,108 @@ namespace toDoList{
         protected int dtstart;
         protected int dtend;
         protected string tag;
-        public task(string name,int taskid, string task_description, bool is_done, int dtstart, int dtend, string tag)
+        public task(string name, int taskid, string task_description, bool is_done, int dtstart, int dtend, string tag)
         {
-        this.name=name;
-        this.taskid=taskid;
-        this.task_description=task_description;
-        this.is_done=is_done;
-        this.dtstart=dtstart;
-        this.dtend=dtend;
-        this.tag=tag;
+            this.name = name;
+            this.taskid = taskid;
+            this.task_description = task_description;
+            this.is_done = is_done;
+            this.dtstart = dtstart;
+            this.dtend = dtend;
+            this.tag = tag;
         }
     }
     class userTask
     {
         protected string order;
-
-        public userTask(string order)
+        protected int tasks;
+        protected task[] taskDetail;
+        public userTask(string userName)
         {
-            
+            this.tasks = 0;
+            int HowMTask = 0;
+            string jsonContent = File.ReadAllText("db.json");
+            JObject dbFile = JObject.Parse(jsonContent);
+            foreach (var user in dbFile["users"])
+            {
+                if (userName == user["username"].ToString())
+                {
+                    foreach (object taskObjectT in user["task"])
+                    {
+                        this.tasks++;
+                    }
+
+                    taskDetail = new task[this.tasks];
+                    int indexT = 0;
+
+                    foreach (var taskObjectT in user["task"])
+                    {
+                        taskDetail[indexT] = new task("aaaa", Convert.ToInt32(taskObjectT["taskId"].ToString()), taskObjectT["taskDes"].ToString(), Convert.ToBoolean(taskObjectT["isDone"].ToString()), Convert.ToInt32(taskObjectT["timeDateStart"].ToString()), Convert.ToInt32(taskObjectT["timeDateDone"].ToString()), taskObjectT["tag"].ToString());
+
+                    }
+
+                }
+            }
+        }
+        public task[] addTask()
+        {
+
+        }
+        public task[] deleteTask()
+        {
+
+        }
+        public int howManyTask()
+        {
+            return this.tasks;
+        }
+        public task[] detailedTask()
+        {
+            return taskDetail;
         }
     }
-    class program{
-        static void Main(string[] args){
+    class program
+    {
+        static void Main(string[] args)
+        {
             //Console.Clear();
-            while(true){
+            while (true)
+            {
                 Console.Write("USER? ");
                 string fuser = Console.ReadLine();
                 Console.Write("PASS? ");
                 string fpass = Console.ReadLine();
-                user loggedInUser = new user(fuser,fpass);
-                while(true){
-                    if(loggedInUser.isLogin() == false){
+                user loggedInUser = new user(fuser, fpass);
+                while (true)
+                {
+                    if (loggedInUser.isLogin() == false)
+                    {
                         Console.WriteLine("WELCOME\n\n1. register user\n2. login user\n3. change user\n4. exit");
                         int userInputMenu = Convert.ToInt32(Console.ReadLine());
-                        if(userInputMenu == 1){
+                        if (userInputMenu == 1)
+                        {
                             loggedInUser.register();
                             Console.WriteLine("try to register");
                         }
-                        if(userInputMenu == 2){
+                        if (userInputMenu == 2)
+                        {
                             loggedInUser.login();
                             Console.WriteLine("Try to login");
                         }
-                        if(userInputMenu == 3){ break; }
-                        if(userInputMenu == 4){
+                        if (userInputMenu == 3) { break; }
+                        if (userInputMenu == 4)
+                        {
                             Console.WriteLine("EXIT PROGRAM");
                             Environment.Exit(0);
                         }
-                    }else{
+                    }
+                    else
+                    {
                         Console.WriteLine($"Logged in as {loggedInUser.getUsername()}\n1. logout");
                         int userInputMenuL = Convert.ToInt32(Console.ReadLine());
-                        if(userInputMenuL == 1){ loggedInUser.logout(); }
+                        userTask ut = new userTask(loggedInUser.getUsername());
+                        Console.WriteLine(ut.howManyTask());
+                        if (userInputMenuL == 1) { loggedInUser.logout(); }
                     }
                 }
             }
